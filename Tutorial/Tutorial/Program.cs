8e2f1a2fc53bdf62903122c704259be87f5e0d29
Jamey Bryce <jbxhalite5@gmail.com>
2015-12-05 11:56:49 -0600

    First C# Project v1. Just a test really, like this whole repository.

---
+++ Tutorial/Tutorial/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Tutorial
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello, I'm a computer.");
+			Console.ReadKey ();
+		}
+	}
+}
