a835b4092a89be8c0c095e8f5e50c08cbc1c0f6c
Jamey Bryce <jbxhalite5@gmail.com>
2015-12-05 12:35:22 -0600

    Updated the C# code. Just playing around :)

--- Tutorial/Tutorial/Program.cs
+++ Tutorial/Tutorial/Program.cs
@@ -6,8 +6,16 @@ namespace Tutorial
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello, I'm a computer.");
+			int num1;
+			int num2;
+
+			Console.Write ("Type in a number to be multiplied: ");
+			num1 = Convert.ToInt32 (Console.ReadLine ());
+			Console.Write ("Type in another number to be multiplied: ");
+			num2 = Convert.ToInt32 (Console.ReadLine ());
+			Console.WriteLine ("The product is: " + num1*num2);
 			Console.ReadKey ();
+			
 		}
 	}
 }
